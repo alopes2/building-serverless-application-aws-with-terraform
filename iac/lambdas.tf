5293174f6f87421bfede3615a70bb92e13de9f17
Andre <andrevitorlopes@gmail.com>
2025-01-11 16:28:34 +0100

    REvert

--- iac/lambdas.tf
+++ iac/lambdas.tf
@@ -11,11 +11,6 @@ resource "aws_iam_role" "lambda_role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_iam_role_policy" "inline_policies" {
-  role   = aws_iam_role.lambda_role.name
-  policy = data.aws_iam_policy_document.lambda_policies.json
-}
-
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
