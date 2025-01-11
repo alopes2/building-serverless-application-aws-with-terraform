a5cf1c1ebf217e09a3b8471da131a349cbb9f1db
Andre <andrevitorlopes@gmail.com>
2025-01-11 16:37:26 +0100

    Revert stuff

--- iac/lambdas.tf
+++ iac/lambdas.tf
@@ -1,3 +1,51 @@
-module "get_data_lambda" {
-  source = "./modules/lambda"
+resource "aws_lambda_function" "lambda" {
+  filename      = data.archive_file.lambda.output_path
+  function_name = "get-data"
+  handler       = "index.handler"
+  runtime       = "nodejs22.x"
+  role          = aws_iam_role.lambda_role.arn
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "get-data-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy" "inline_policies" {
+  role   = aws_iam_role.lambda_role.name
+  policy = data.aws_iam_policy_document.lambda_policies.json
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "./lambda_init_code/index.js"
+  output_path = "get-data_lambda_function_payload.zip"
+}
+
+data "aws_iam_policy_document" "lambda_policies" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
